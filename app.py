eb2ca2c7f3b282a24a497257f3762e981ff62a4e
unknown <pshkmr007@gmail.com>
2021-07-14 14:56:23 +0530

    working

--- app.py
+++ app.py
@@ -69,12 +69,44 @@ def index():
     # df= pd.read_sql("select * from Table2", conn)
     return render_template('index.html',task=zip(list1,sdatelist,edatelist),graytb1= zip(listtb1,stb1datelist,etb1datelist))
 
-@app.route('/filter',methods=['POST'])
+@app.route('/filter',methods=['GET','POST'])
 def filter():
-    task = cursor.execute("select * from Table2 where Taskname in ('T1','T2') and OS = 'Windows';")
-    print(request.form)
-    return "Reached!!"
-    
+    table1 = cursor.execute('SELECT * FROM Test_GanttChart.dbo.Table1')
+    # print(table1)
+    listtb1=list()
+    stb1datelist = list()
+    etb1datelist = list()
+    for data in table1:
+        listtb1.append(data)
+        # startdate
+        date_time_obj = datetime.datetime.strptime(data[3], '%Y-%m-%d')
+        sdate = datetime.datetime.timestamp(date_time_obj)
+        stb1datelist.append(round(sdate * 1000 ))
+        #enddate
+        date_time_obj = datetime.datetime.strptime(data[4], '%Y-%m-%d')
+        edate = datetime.datetime.timestamp(date_time_obj)
+        etb1datelist.append(round(edate * 1000))
+     
+    table2 = cursor.execute('SELECT * FROM Test_GanttChart.dbo.Table2') 
+    taskgrp = list()
+    list1=list()
+    sdatelist = list()
+    edatelist = list()
+    for row in table2:
+        list1.append(row)
+        # startdate
+        taskgrp.append(row[1])
+        # print(row[1])
+        date_time_obj = datetime.datetime.strptime(row[4], '%Y-%m-%d')
+        sdate = datetime.datetime.timestamp(date_time_obj)
+        sdatelist.append(round(sdate * 1000 ))
+        #enddate
+        date_time_obj = datetime.datetime.strptime(row[5], '%Y-%m-%d')
+        edate = datetime.datetime.timestamp(date_time_obj)
+        edatelist.append(round(edate * 1000))
+    # df= pd.read_sql("select * from Table2", conn)
+    # for i in zip(list1,))
+    return render_template('base.html',task=sorted(set(taskgrp)),graytb1= zip(listtb1,stb1datelist,etb1datelist))
 
 if __name__ == "__main__":
     app.run()
